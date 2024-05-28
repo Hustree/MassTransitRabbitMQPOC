d2c9a405e196cfd21c0d2c2c0b8399d6f1fe891f
Joshua Bascos <joshuabascos@gmail.com>
2024-05-28 18:04:42 +0800

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,46 @@
+﻿using MassTransit;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System.Threading.Tasks;
+
+namespace MassTransitRabbitMQPOC
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var host = CreateHostBuilder(args).Build();
+
+            if (args.Length > 0 && args[0] == "publish")
+            {
+                await Publisher.Publish(host);
+            }
+            else
+            {
+                await host.RunAsync();
+            }
+        }
+
+        public static IHostBuilder CreateHostBuilder(string[] args) =>
+            Host.CreateDefaultBuilder(args)
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddMassTransit(x =>
+                    {
+                        x.AddConsumer<MessageConsumer>();
+
+                        x.UsingRabbitMq((context, cfg) =>
+                        {
+                            cfg.Host("localhost", "/", h => { });
+
+                            cfg.ReceiveEndpoint("message-queue", e =>
+                            {
+                                e.ConfigureConsumer<MessageConsumer>(context);
+                            });
+                        });
+                    });
+
+                    //services.AddMassTransitHostedService();
+                });
+    }
+}
---
+++ Subscriber.cs
@@ -0,0 +1,15 @@
+﻿using MassTransit;
+using System;
+using System.Threading.Tasks;
+
+namespace MassTransitRabbitMQPOC
+{
+    public class MessageConsumer : IConsumer<Message>
+    {
+        public Task Consume(ConsumeContext<Message> context)
+        {
+            Console.WriteLine($"Received: {context.Message.Text}");
+            return Task.CompletedTask;
+        }
+    }
+}
